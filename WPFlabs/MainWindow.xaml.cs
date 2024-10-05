2041dd37ae5bc289537b4740de3e43aff70a641c
xom9kxom9k <vfomicev586@gmail.com>
2024-10-05 14:56:44 +0400

    завершение лабораторных работ

--- WPFlabs/MainWindow.xaml.cs
+++ WPFlabs/MainWindow.xaml.cs
@@ -24,5 +24,54 @@ namespace WPFlabs
         {
             InitializeComponent();
         }
+        private void But_Play_Casino(object sender, RoutedEventArgs e)
+        {
+            casino.Play();
+        }
+        private void But_Stop_Casino(object sender, RoutedEventArgs e)
+        {
+            casino.Stop();
+        }
+
+        private void But_Play_Tamaev(object sender, RoutedEventArgs e)
+        {
+            tamaev.Play();
+        }
+        private void But_Stop_Tamaev(object sender, RoutedEventArgs e)
+        {
+            tamaev.Stop();
+        }
+        private void vol_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            casino.Volume = (double)vol.Value;
+        }
+        private void vol_ValueChanged2(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            tamaev.Volume = (double)vol2.Value;
+        }
+        private void playbutton1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("5 красное");
+        }
+        private void playbutton2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ОГО, ровно 21");
+        }
+        private void playbutton3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Бонуска не окупилась");
+        }
+        private void playbutton4(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Флеш рояль");
+        }
+        private void playbutton5(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ни как у Тамаева конечно, но тоже нормально");
+        }
+        private void playbutton6(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Тяните карту");
+        }
     }
 }
